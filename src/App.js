b89c067d0830878172cf8417703f10c89e430313
mansuroff123 <mansuroff145@gmail.com>
2024-09-28 11:02:38 +0500

    to-do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState('')
+
+  // Yangi vazifa qo'shish
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      const task = {
+        id: Date.now(),
+        text: newTask,
+        completed: false
+      };
+      setTasks([...tasks, task]);
+      setNewTask(''); // Task Qo'shilgandan keyin inputni tozalash
+    }
+  }
+
+  // Vazifani bajarilgan deb belgilash
+  const toggleTaskCompletion = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? {...task, completed: !task.completed} : task
+      )
+    );
+  };
+
+  // Vazifani o'chirish
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>To-do List</h1>
+      <div className='input-container'>
+        <input 
+        type='text' 
+        placeholder="Vazifa qo'shish" 
+        onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button>Qo'shish</button>
+      </div>
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id} className={task.completed ? 'completed' : ''}>
+            {task.text}
+            <button onClick={() => toggleTaskCompletion(task.id)}>
+              {task.completed ? 'Bajarilmadi': 'Bajarildi'}
+            </button>
+            <button onClick={() => deleteTask(task.id)}>O'chirish</button>
+          </li>
+        ))
+
+        }
+      </ul>
     </div>
   );
 }
